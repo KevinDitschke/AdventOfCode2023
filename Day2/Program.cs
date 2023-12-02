e8bf6330efb5de12fed88590ab8e0afcb7b6cc8e
Kevin Ditschke <kevin.ditschke@hotmail.de>
2023-12-02 12:39:15 +0100

    Solved Day 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace Day2; 
+namespace Day2;
 
 internal class Program
 {
@@ -10,23 +10,46 @@ internal class Program
         var lines = input.Split('\n');
         var idPattern = @"(\d+)";
         var numberPattern = @"\d+";
+        var lowestMultiplied = 0;
         var ids = Enumerable.Range(1, lines.Length).ToList();
-        var limitDictionary = new Dictionary<string, int>() { {"red", 12}, {"green", 13}, {"blue", 14} };
+        var limitDictionary = new Dictionary<string, int>() { { "red", 12 }, { "green", 13 }, { "blue", 14 } };
         foreach (var line in lines)
         {
             var sets = line.Split(':')[1].Split(';');
+            var lowestRed = 0;
+            var lowestGreen = 0;
+            var lowestBlue = 0;
             foreach (var set in sets)
             {
                 var contents = set.Split(',');
+
                 foreach (var content in contents)
                 {
                     var count = Int32.Parse(Regex.Match(content, numberPattern).Value);
                     var color = Regex.Match(content, @"(red)|(green)|(blue)").Value;
+                    switch (color)
+                    {
+                        case "red":
+                            if (count > lowestRed)
+                                lowestRed = count;
+                            break;
+                        case "green":
+                            if (count > lowestGreen)
+                                lowestGreen = count;
+                            break;
+                        case "blue":
+                            if (count > lowestBlue)
+                                lowestBlue = count;
+                            break;
+                    }
+
                     if (limitDictionary[color] < count)
                         ids.Remove(Int32.Parse(Regex.Match(line, idPattern).Value));
                 }
             }
+            lowestMultiplied += lowestRed * lowestGreen * lowestBlue;
         }
         Console.WriteLine(ids.Sum());
+        Console.WriteLine(lowestMultiplied);
     }
 }
