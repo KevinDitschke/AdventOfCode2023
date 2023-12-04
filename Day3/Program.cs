c5d7d559714cfc4b25f0f0cd7f874274d893d0d8
k.ditschke <l.ditschke@schenckprocess.com>
2023-12-04 16:18:15 +0100

    Solved Day 3 Part 1

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,25 +1,67 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Day3;
 
 internal class Program
 {
+    private const string specialCharPattern = @"[^0-9.\r\n]";
+
     static async Task Main(string[] args)
     {
         var input = await File.ReadAllTextAsync("Input.txt");
-        var numberRegex = @"\d+";
-        var numbers = Regex.Matches(input, numberRegex).Select(x => Int32.Parse(x.Value)).ToList();
         var array = input.Split('\n').Select(x => x.ToCharArray()).ToArray();
-        for (int i = 0; i < array.Length; i++)
+        var total = 0;
+        for (var i = 0; i < array.Length; i++)
         {
-            for (int j = 0; j < array[i].Length; j++)
+            var numberBuilder = new StringBuilder();
+            var isAdjacent = false;
+            for (var j = 0; j < array[i].Length; j++)
             {
                 if (!char.IsDigit(array[i][j]))
+                {
+                    if (numberBuilder.Length != 0 && isAdjacent)
+                        total += int.Parse(numberBuilder.ToString());
+                    numberBuilder = new StringBuilder();
+                    isAdjacent = false;
                     continue;
-                
-                
+                }
+
+                numberBuilder.Append(array[i][j]);
+                if (numberBuilder.ToString() == "409")
+                    Console.WriteLine("409!!!!!");
+                if (IsAdjacent(array, i, j + 1)
+                    || IsAdjacent(array, i - 1, j + 1)
+                    || IsAdjacent(array, i, j - 1)
+                    || IsAdjacent(array, i + 1, j + 1)
+                    || IsAdjacent(array, i + 1, j)
+                    || IsAdjacent(array, i - 1, j + 1)
+                    || IsAdjacent(array, i + 1, j - 1)
+                    || IsAdjacent(array, i - 1, j - 1))
+                    isAdjacent = true;
+
                 Console.WriteLine($"{i}, {j}: {array[i][j]}");
             }
         }
+
+        Console.WriteLine(total);
+    }
+
+    private static bool IsAdjacent(char[][] array, int row, int column)
+    {
+        try
+        {
+            if (Regex.Match(array[row][column].ToString(), specialCharPattern).Success)
+            {
+                Console.WriteLine($"Matching symbol:{array[row][column]}");
+                return true;
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+            return false;
+        }
+
+        return false;
     }
 }
